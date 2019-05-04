25cef3065fca58929643879fc0ce972431aa877a
xyh <467946553@qq.com>
2019-05-04 22:21:40 +0800

    feat:完成了AdminBasicFuncServiceImpl和ContactServiceImpl里的方法

--- src/main/java/psychology/service/AdminBasicFuncServiceImpl.java
+++ src/main/java/psychology/service/AdminBasicFuncServiceImpl.java
@@ -1,24 +1,38 @@
 package psychology.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import psychology.bl.AdminBasicFuncService;
+import psychology.dao.UserRepo;
 import psychology.entity.User;
 import psychology.response.BasicResponse;
 
+import java.util.List;
+
 @Service
 public class AdminBasicFuncServiceImpl implements AdminBasicFuncService {
+    @Autowired
+    private UserRepo userRepo;
 
     @Override
     public User[] getUserInfo(){
-        //TODO
-
-        return null;
+        List<User> userList = userRepo.findAll();
+        User[] users = new User[userList.size()];
+        for(int i=0;i<userList.size();i++){
+            users[i] = userList.get(i);
+        }
+        return users;
     }
 
     @Override
     public BasicResponse cancelUser(String userEmail){
-        //TODO
-
-        return null;
+        User user = null;
+        if(userRepo.findById(userEmail).isPresent()){
+            user = userRepo.findById(userEmail).get();
+            userRepo.delete(user);
+            return new BasicResponse(true,"");
+        }else {
+            return new BasicResponse(false,"该用户邮箱不存在！");
+        }
     }
 }
--- src/main/java/psychology/service/ContactServiceImpl.java
+++ src/main/java/psychology/service/ContactServiceImpl.java
@@ -1,31 +1,48 @@
 package psychology.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import psychology.bl.ContactService;
+import psychology.dao.ContactRepo;
 import psychology.entity.Contact;
 import psychology.response.BasicResponse;
 
+import java.util.List;
+
 @Service
 public class ContactServiceImpl implements ContactService {
+    @Autowired
+    private ContactRepo contactRepo;
 
     @Override
     public Contact[] getContact(){
-        //TODO
-
-        return null;
+        List<Contact> contactList = contactRepo.findAll();
+        Contact[] contacts = new Contact[contactList.size()];
+        for(int i=0;i<contactList.size();i++){
+            contacts[i] = contactList.get(i);
+        }
+        return contacts;
     }
 
     @Override
     public BasicResponse addContact(String name, String email, String mobileNum){
-        //TODO
-
-        return null;
+        if(contactRepo.findById(name).isPresent()){
+            return new BasicResponse(false,"该联系人的姓名已存在！");
+        }else {
+            Contact contact = new Contact(name,email,mobileNum);
+            contactRepo.save(contact);
+            return new BasicResponse(true,"");
+        }
     }
 
     @Override
     public BasicResponse deleteContact(String name){
-        //TODO
-
-        return null;
+        if(contactRepo.findById(name).isPresent()){
+            Contact contact = contactRepo.findById(name).get();
+            contactRepo.delete(contact);
+            return new BasicResponse(true,"");
+        }else {
+            return new BasicResponse(false,"该联系人的姓名不存在！");
+        }
     }
 }
